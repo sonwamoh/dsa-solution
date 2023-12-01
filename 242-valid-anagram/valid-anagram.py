429730b091338b0ce5b081d1a596a4123bf0467b
Mohit Sonwane <101231709+TechGeek-Mohit@users.noreply.github.com>
2023-12-01 13:04:13 -0800

    Time: 56 ms (47.04%) | Memory: 17 MB (29.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,5 +5,15 @@ class Solution:
         TC: O(n)
         SC: O(1)
         """
-        return ''.join(sorted(s)) == ''.join(sorted(t))
+        if len(s) != len(t):
+            return False
+        s_count = {}
+        t_count = {}
+        for s1, t1 in zip(s, t):
+            s_count[s1] = s_count.get(s1, 0) + 1
+            t_count[t1] = t_count.get(t1, 0) + 1
+        print(s_count, t_count)
+        return s_count == t_count
+
+        
         
