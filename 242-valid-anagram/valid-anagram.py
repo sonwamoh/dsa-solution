a636cbe95d75e24e3bfb0a9fa07ca5e27fba6301
Mohit Sonwane <101231709+sonwamoh@users.noreply.github.com>
2024-10-02 22:38:25 -0700

    Time: 41 ms (88.65%) | Memory: 17.1 MB (28.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        """
-        Optimize for space 
-        TC: O(n)
-        SC: O(n)
-        """
-        if len(s) != len(t):
-            return False
-        s_count = {}
-        t_count = {}
-        for s1, t1 in zip(s, t):
-            s_count[s1] = s_count.get(s1, 0) + 1
-            t_count[t1] = t_count.get(t1, 0) + 1
-        print(s_count, t_count)
-        return s_count == t_count
-
-        
+        return Counter(s) == Counter(t)
         
