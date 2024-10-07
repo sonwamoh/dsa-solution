e4806917a1a84cae532cac89acb72f4198e0cc56
Mohit Sonwane <101231709+sonwamoh@users.noreply.github.com>
2024-10-06 17:07:37 -0700

    Time: 482 ms (48.22%) | Memory: 28.9 MB (95.29%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -9,15 +9,10 @@ class Solution:
         len(n) > k
         [1,2,3,4]
         """
-        min_heap = []
-        
-        for num in nums:
-            if len(min_heap) < k:
-                heapq.heappush(min_heap, num)
-            else:
-                heapq.heappushpop(min_heap, num)
-        
-        return min_heap[0]
+        heapq.heapify(nums)
+        while len(nums) > k:
+            heapq.heappop(nums)
+        return nums[0]
 
         
 
